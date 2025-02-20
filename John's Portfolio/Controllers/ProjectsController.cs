809100eaf6670c50815b6fb61efbd02fbf91d994
SevenofThr4wn <johneley798@vgmail.com>
2025-02-21 09:47:44 +1000

    Edited Base Files

---
+++ John's Portfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace John_s_Portfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
