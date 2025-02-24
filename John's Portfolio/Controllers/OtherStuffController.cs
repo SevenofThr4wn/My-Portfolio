54c352e09f45251119fcd558e0d668f5bdebf521
SevenofThr4wn <johneley798@vgmail.com>
2025-02-24 10:52:49 +1000

    Added new Controller and view

---
+++ John's Portfolio/Controllers/OtherStuffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace John_s_Portfolio.Controllers
+{
+    public class OtherStuffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
